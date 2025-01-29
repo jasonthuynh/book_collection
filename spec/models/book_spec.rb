c192a597de9c5dd237267ef119d650e22a5cd4d4
jasonthuynh <jasonhuynh512@gmail.com>
2025-01-29 01:52:49 +0000

    added integration testing for part 1

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,4 @@
-# MY UNIT TESTS
+######## MY UNIT TESTS #########
 
 require 'rails_helper'
 
@@ -13,3 +13,24 @@ RSpec.describe Book, type: :model do
     expect(book).not_to be_valid
   end
 end
+
+######## MY INTEGRATION TESTS ########
+
+RSpec.describe "Book management", type: :feature do
+    scenario "user successfully adds a new book" do
+      visit new_book_path
+      fill_in "Title", with: "Test Book"
+      click_button "Create Book"
+  
+      expect(page).to have_content("Book was successfully created.")
+      expect(Book.last.title).to eq("Test Book")
+    end
+  
+    scenario "user is unable to add a new book without a title" do
+      visit new_book_path
+      fill_in "Title", with: ""
+      click_button "Create Book"
+  
+      expect(page).to have_content("Title can't be blank")
+    end
+  end
